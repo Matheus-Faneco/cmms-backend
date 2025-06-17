6c63c3a946f32c7acd91ccb74815d43f21b6711b
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2025-06-17 16:41:22 -0300

    documentacao de api com swagger e redoc usando drf-yasg

--- cmms/urls.py
+++ cmms/urls.py
@@ -17,12 +17,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CMMS",
+        default_version='v1',
+        description="Documentação de API do sistema CMMS",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/maintenance/', include('maintenance.urls')),
-    path('api/machines/', include('machines.urls'))
+    path('api/machines/', include('machines.urls')),
+
+    # Swagger e Redoc
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),  # ✅ Corrigido
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
