fcbf7745f9a9d96b42e0eb695d8742144f0baf3e
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2025-06-17 16:52:56 -0300

    integracao com Redis para cache de consultas de maquinas

--- machines/urls.py
+++ machines/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .viewsets import MachineSerializer, MachineViewSet
+from .views import cached_machine_list
+from .viewsets import MachineViewSet
 
 router = DefaultRouter()
 
 router.register('machines', MachineViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('machines/cached', cached_machine_list, name='cached-machine-list')
 ]
 
--- machines/views.py
+++ machines/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.core.cache import cache
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from machines.models import Machine
 
-# Create your views here.
+@api_view(['GET'])
+def cached_machine_list(request):
+    data = cache.get('machine_list')
+
+    if data is None:
+        data = list(Machine.objects.values())
+        cache.set('machine_list', data, timeout=60)
+
+    return Response(data)
+
