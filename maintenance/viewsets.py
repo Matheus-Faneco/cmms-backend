0cde598fe438b0614e666c39b561ba1e9faa6460
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2025-06-17 16:30:42 -0300

    adicao de django_filter nas manutencoes

--- maintenance/viewsets.py
+++ maintenance/viewsets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from .models import MaintenanceOrder
 from .serializers import MaintenanceOrderSerializer
@@ -5,3 +6,5 @@ from .serializers import MaintenanceOrderSerializer
 class MaintenanceOrderViewSet(viewsets.ModelViewSet):
     queryset = MaintenanceOrder.objects.all()
     serializer_class = MaintenanceOrderSerializer
+    filter_backend = [DjangoFilterBackend]
+    filterset_fields = ['tx_status', 'tx_type', 'machine']
